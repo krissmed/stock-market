6b8f47310c02f1f509255d2125b7305111f76d41
Kristian Smedsrød <krissmed@protonmail.com>
2022-10-27 23:57:56 +0200

    HamburgerMenu is now insertet Only the left menu is used

--- stock-market/ClientApp/src/components/RespNav.jsx
+++ stock-market/ClientApp/src/components/RespNav.jsx
@@ -1,9 +1,10 @@
-﻿import * as React from 'react';
+﻿/// <reference path="nav.jsx" />
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
+import { Link } from "react-router-dom";
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
 import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
@@ -11,51 +12,41 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
+import Logo from '../assets/LOGO.svg';
+import { useTheme, styled } from '@mui/material/styles';
+
+
 
 export default function RespNav() {
-    const [state, setState] = React.useState({
-        top: false,
-        left: false,
-        bottom: false,
-        right: false,
-    });
+    const customTheme = useTheme();
+    const [drawer, setDrawer] = useState(false)
 
-    const toggleDrawer = (anchor, open) => (event) => {
+    const toggleDrawer = (open) => (event) => {
         if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
             return;
         }
 
-        setState({ ...state, [anchor]: open });
+        setDrawer(open);
     };
 
-    const list = (anchor) => (
+    const list = () => (
         <Box
-            sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
             role="presentation"
-            onClick={toggleDrawer(anchor, false)}
-            onKeyDown={toggleDrawer(anchor, false)}
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
         >
+                    <Link to="/"> 
+                        <img src={Logo} alt="LOGO" />
+                    </Link>
+                
             <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                {['Dashboard', 'Buy Stocks', 'Portfolio', 'Watchlist', 'Transactions'].map((text, index) => (
                     <ListItem key={text} disablePadding>
                         <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-            <Divider />
-            <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
+                                <ListItemIcon>
+                                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                </ListItemIcon>
+                                <ListItemText primary={text} />
                         </ListItemButton>
                     </ListItem>
                 ))}
@@ -65,18 +56,15 @@ export default function RespNav() {
 
     return (
         <div>
-            {['left', 'right', 'top', 'bottom'].map((anchor) => (
-                <React.Fragment key={anchor}>
-                    <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+            <React.Fragment>
+                <MenuIcon onClick={toggleDrawer(true)} sx={{ color: 'white' }} />
                     <Drawer
-                        anchor={anchor}
-                        open={state[anchor]}
-                        onClose={toggleDrawer(anchor, false)}
+                        open={drawer}
+                        onClose={toggleDrawer(false)}
                     >
-                        {list(anchor)}
+                        {list()}
                     </Drawer>
                 </React.Fragment>
-            ))}
         </div>
     );
 }
