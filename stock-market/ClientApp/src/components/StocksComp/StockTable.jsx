9c2f75de614525a8398ede8cf75d1fb38c8d1392
Ole Jørgen Knoph <s354371@oslomet.no>
2022-10-27 20:22:19 +0200

    Implementert Buy, Delete, Add STOCK. Samt gjort grafen funksjonabel og presentabel

--- stock-market/ClientApp/src/components/StocksComp/StockTable.jsx
+++ stock-market/ClientApp/src/components/StocksComp/StockTable.jsx
@@ -1,8 +1,8 @@
-﻿import React from 'react';
+﻿import React from 'react';  
 import { useState } from 'react';
 import axios from 'axios';
 
-import StockGraph from "./StockGraph";
+import StockGraphData from "../../fetchingData/StockGraphData";
 
 import {useTheme} from '@mui/material/styles';
 import Table from '@mui/material/Table';
@@ -19,12 +19,14 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import CircularProgress from '@mui/material/CircularProgress';
+
 
 //Object to store variables showed. Fetched data;
 
 //Customized to enable expandable row when icon is clicked
 
-const ExpandableRows = ({ children, curStock, ...otherArgs}) => {
+const ExpandableRows = ({ children, curStock, ...otherArgs }) => {
 
     //Hook to determine if expanded and set it to expanded or not;
     const [isExpanded, setIsExpanded] = useState(false);
@@ -42,10 +44,10 @@ const ExpandableRows = ({ children, curStock, ...otherArgs}) => {
                         padding: 3,
                         fontSize: 10
                     }}
-                                onClick={() => setIsExpanded(!isExpanded)}>
+                        onClick={() => setIsExpanded(!isExpanded)}>
                         {isExpanded ?
-                            <KeyboardArrowUpIcon sx={{fontSize: 35}} />
-                            : <KeyboardArrowDownIcon sx={{fontSize: 35}}/>}
+                            <KeyboardArrowUpIcon sx={{ fontSize: 35 }} />
+                            : <KeyboardArrowDownIcon sx={{ fontSize: 35 }} />}
                     </IconButton>
                 </TableCell>
             </TableRow>
@@ -54,9 +56,9 @@ const ExpandableRows = ({ children, curStock, ...otherArgs}) => {
                 <TableRow>
                     <TableCell padding="checkbox" />
                     <TableCell colSpan="5">
-                        <StockGraph stock={curStock}/>
+                        <StockGraphData ticker={curStock.ticker} />
                     </TableCell>
-                    <TableCell padding="checkbox"/>
+                    <TableCell padding="checkbox" />
                 </TableRow>
             )}
 
@@ -65,132 +67,162 @@ const ExpandableRows = ({ children, curStock, ...otherArgs}) => {
     );
 }
 
-//Functions to execute the buttons
 
-function buyStock(ticker) {
-    const amount = prompt("Please enter desired amount to buy", 1);
-
-    axios.get('transaction/buystock?ticker=' + ticker + '&amount=' + amount)
-        .then((response) => {
-            if (response) {
-                alert('Successfully bought ' + amount + ' shares of ' + ticker);
-            }
-            else {
-                alert('You dont have enough money...;/');
-            }
-        })
-}
-
-function deleteStock(ticker) {
-    axios.get('stock/deletestock?ticker=' + ticker)
-        .then((response) => {
-            if (response) {
-                window.location.reload();
-                alert("Successfull deletion");
-            }
-        })
-}
-
-function addStock() {
-    const chosenTicker = prompt("Enter the ticker of the stock you want to add", 'GOOGL').toUpperCase();
-
-    axios.get('stock/addstock?ticker=' + chosenTicker)
-        .then((response) => {
-            if (response) {
-                window.location.reload();
-                alert('Successfully added ' + chosenTicker);
-            }
-        })
-}
 
 //Component to list out the table with the data
 
 export default function StockTable({ stockObj }) {
+    //To check if loading
+    const [isLoading, setIsLoading] = useState(false);
+
+    //Functions to execute the buttons
+
+    function buyStock(ticker) {
+        const amount = prompt("Please enter desired amount to buy", 1);
+
+        setIsLoading(true);
+        axios.get('transaction/buystock?ticker=' + ticker + '&amount=' + amount)
+            .then((response) => {
+                if (response) {
+                    alert('Successfully bought ' + amount + ' shares of ' + ticker);
+                }
+                else {
+                    alert('You dont have enough money...;/');
+                }
+                setIsLoading(false);
+            })
+    }
+
+    function deleteStock(ticker) {
+        setIsLoading(true);
+        axios.get('stock/deletestock?ticker=' + ticker)
+            .then((response) => {
+                if (response) {
+                    window.location.reload();
+                    setIsLoading(false);  
+                }
+                else {
+                    alert("Something wrong. Ask Erling");
+                }
+            })
+    }
+
+    function addStock() {
+        const chosenTicker = prompt("Enter the ticker of the stock you want to add", 'GOOGL').toUpperCase();
+
+        setIsLoading(true);
+        axios.get('stock/addstock?ticker=' + chosenTicker)
+            .then((response) => {
+                if (response) {
+                    window.location.reload();
+                    setIsLoading(false);
+                }
+                else {
+                    alert("Something wrong. Ask Erling");
+                }
+            })
+    }
 
     const customTheme = useTheme();
 
     return (
-        <Box sx={{
-            width: 'auto',
-            overflowX: 'auto',
+        
+            isLoading
+            ?
 
-        }}>
-            <Button variant='contained' color='success'
-                    onClick={() => addStock()}
-                    sx={{
-                        float: 'right',
-                        mt: 1,
-                        mx: 3
-                    }}>
-                <Typography variant='subtitle1'>
-                    + Add Stock
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: 'center'
+                }}>
+                <CircularProgress color="success" />
+            </div>
+
+                :
+
+    <Box sx={{
+        width: 'auto',
+        overflowX: 'auto',
+
+    }}>
+        <Button variant='contained' color='success'
+            onClick={() => addStock()}
+            sx={{
+                float: 'right',
+                mt: 1,
+                mx: 3
+            }}>
+            <Typography variant='subtitle1'>
+                + Add Stock
                 </Typography>
-            </Button>
-            <Table aria-label='Table with all Stocks'>
-                <TableHead>
-                    <TableRow>
-                        <TableCell padding='checkbox'/>
+        </Button>
+        <Table aria-label='Table with all Stocks'>
+            <TableHead>
+                <TableRow>
+                    <TableCell padding='checkbox' />
+                    <TableCell align="right">
+                        <Typography variant='h6'
+                            color={customTheme.palette.primary.contrastText}> Name </Typography>
+                    </TableCell>
+                    <TableCell align="right" sx={{ textAlign: 'center' }}>
+                        <Typography variant='h6' color={customTheme.palette.primary.contrastText}>Value</Typography>
+                    </TableCell>
+                    <TableCell padding='checkbox' />
+                    <TableCell padding='checkbox' />
+                    <TableCell padding="checkbox" />
+                </TableRow>
+            </TableHead>
+
+            <TableBody>
+                {stockObj.map(stock => (
+
+                    <ExpandableRows
+                        key={stock.ticker}
+                        curStock={stock}
+                    >
+                        <TableCell sx={{ maxWidth: 2 }}>
+                            <Typography variant='body1' color={customTheme.palette.primary.contrastText}>
+                                {stock.ticker}
+                            </Typography>
+                        </TableCell>
+
                         <TableCell align="right">
-                            <Typography variant='h6'
-                                        color={customTheme.palette.primary.contrastText}> Name </Typography>
+                            <Typography variant='body1' color={customTheme.palette.primary.contrastText}>
+                                {stock.name}
+                            </Typography>
                         </TableCell>
-                        <TableCell align="right" sx={{textAlign:'center'}}>
-                            <Typography variant='h6' color={customTheme.palette.primary.contrastText}>Value</Typography>
+
+                        <TableCell align="right" sx={{ textAlign: 'center' }}>
+                            <Typography variant='body1' color={customTheme.palette.primary.contrastText}>
+                                {stock.current_price} $
+                                </Typography>
                         </TableCell>
-                        <TableCell padding='checkbox'/>
-                        <TableCell padding='checkbox'/>
-                        <TableCell padding="checkbox"/>
-                    </TableRow>
-                </TableHead>
 
-                <TableBody>
-                    {stockObj.map(stock => (
-
-                        <ExpandableRows
-                            key={stock.ticker}
-                            curStock={stock}
-                        >
-                            <TableCell sx={{maxWidth: 2}}>
-                                <Typography variant='body1' color={customTheme.palette.primary.contrastText}>
-                                    {stock.ticker}
-                                </Typography>
-                            </TableCell>
-
-                            <TableCell align="right">
-                                <Typography variant='body1' color={customTheme.palette.primary.contrastText}>
-                                    {stock.name}
-                                </Typography>
-                            </TableCell>
-
-                            <TableCell align="right" sx={{textAlign:'center'}}>
-                                <Typography variant='body1' color={customTheme.palette.primary.contrastText}>
-                                    {stock.current_price} $
-                                </Typography>
-                            </TableCell>
-
-                            <TableCell align="right">
-                                <Button variant='outlined'
-                                        color='success'
-                                        endIcon={<AddShoppingCartOutlinedIcon />}
-                                        onClick={() => buyStock(stock.ticker)}>
-                                    Buy
+                        <TableCell align="right">
+                            <Button variant='outlined'
+                                color='success'
+                                endIcon={<AddShoppingCartOutlinedIcon />}
+                                onClick={() => buyStock(stock.ticker)}>
+                                Buy
                                 </Button>
-                            </TableCell>
+                        </TableCell>
 
-                            <TableCell align="right">
-                                <Button variant='outlined'
-                                        color='error'
-                                        endIcon={<DeleteOutlineOutlinedIcon />}
-                                        onClick={() => deleteStock(stock.ticker)}>
-                                    Delete
+                        <TableCell align="right">
+                            <Button variant='outlined'
+                                color='error'
+                                endIcon={<DeleteOutlineOutlinedIcon />}
+                                onClick={() => deleteStock(stock.ticker)}>
+                                Delete
                                 </Button>
-                            </TableCell>
+                        </TableCell>
 
-                        </ExpandableRows>
-                    ))}
+                    </ExpandableRows>
+                ))}
 
-                </TableBody>
-            </Table>
-        </Box>
+            </TableBody>
+        </Table>
+    </Box>
+
     );
 }
--- stock-market/ClientApp/src/fetchingData/StocksData.jsx
+++ stock-market/ClientApp/src/fetchingData/StocksData.jsx
@@ -2,50 +2,58 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-import { CircularProgress } from "@mui/material";
+import CircularProgress from '@mui/material/CircularProgress';
 
 import StockTable from '../components/StocksComp/StockTable';
 
 
 export default function StockData() {
+    const [historicHook, setHistoricHook] = useState([{
+        price: null,
+        time: null
+    }])
 
+    const [dependency, setDependency] = useState(false);
 
     const [stocks, setStocks] = useState([{
         id: null,
         ticker: "",
         name: "",
         current_price: null,
+        history: historicHook
     }]);
-    const [fetching, setFetching] = useState('true');
 
-    
-    //Fetching all stocks with axios get-request; Effect to update once on new render;
+    const [fetching, setFetching] = useState(false);
+
+    console.log(fetching + " init")
+
+    //Fetching all stocks with axios get-request; Effect to update once on mount;
 
     useEffect(() => {
+        setFetching(true);
 
         axios.get('stock/getstocks')
             .then((res) => {
                 setStocks(res.data);
+                
             })
 
         setFetching('false');
 
-    }, []);
-
+    }, [dependency]);
 
     return (
-        fetching
+        fetching === true
             ?
-           <StockTable stockObj = { stocks } />
-            :
-         
-        < div
+            <div
             style = {{
                 display: "flex",
                     justifyContent: "center",
                     alignItems: 'center'
                 }}>
-                <CircularProgress />
+                <CircularProgress color="success" />
             </div>
+            :
+           <StockTable stockObj = { stocks } />
     )
 }
