ebe9fc1c3429e7021a8cb06716ceb9b0e02fa42d
Vetle Endrerud <endrerudvetle@gmail.com>
2022-10-30 21:13:56 +0100

    Created Portfolio
    Added all files.
    Basic design
    Missing GET POST

---
+++ stock-market/ClientApp/src/components/PortfolioComp/PortfolioTable.jsx
@@ -0,0 +1,127 @@
+﻿import React from 'react';
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+
+import { useTheme } from '@mui/material/styles';
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    //Change color on hover
+    '&:hover': {
+        backgroundColor: theme.palette.primary.light,
+    },
+    // hide last border.
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+    textAlign: {
+
+    }
+}));
+
+export default function PortfolioTable({ data }) {
+    const customTheme = useTheme();
+
+    if (data.length == 0) {
+        return (
+            <Box sx={{ textAlign: 'center', mt: 2, height: 500 + 'px' }}>
+                <Typography variant='h6' color={customTheme.palette.primary.contrastText}>There is no current transactions... </Typography>
+            </Box>
+        );
+    }
+
+    //Inputtig into different array to display seperate
+    const buy = [];
+    const sell = []
+
+    data.map((item) => {
+
+        //Converting to readable datetime
+
+        item.timestampid = new Date(item.timestampid).toDateString();
+        if (item.type == 'BUY') {
+            buy.push(item)
+        }
+        else {
+            sell.push(item)
+        }
+    });
+
+    return (
+        <Table sx={{ minWidth: 400 }}
+            sx={{
+                color: customTheme.palette.primary.contrastText
+            }}
+        >
+            <TableHead>
+                <Typography variant='subtitle1' color={customTheme.palette.primary.contrastText}>Your Stocks:</Typography>
+            </TableHead>
+            <TableHead>
+                <TableRow>
+                    <TableCell>
+                        <Typography variant='subtitle2' color={customTheme.palette.primary.contrastText}>
+                            Ticker
+                        </Typography>
+                    </TableCell>
+                    <TableCell>
+                        <Typography variant='subtitle2' color={customTheme.palette.primary.contrastText}>
+                            Quantity
+                        </Typography>
+                    </TableCell>
+                    <TableCell>
+                        <Typography variant='subtitle2' color={customTheme.palette.primary.contrastText}>
+                            Price
+                        </Typography>
+                    </TableCell>
+                    <TableCell>
+                        <Typography variant='subtitle2' color={customTheme.palette.primary.contrastText}>
+                            Sell
+                        </Typography>
+                    </TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {buy.map((row) => (
+                    <StyledTableRow
+                        key={row.Id}
+                    >
+                        <TableCell align="right">
+                            <Typography variant='body2' color={customTheme.palette.primary.contrastText} align='left'>
+                                {row.ticker}
+                            </Typography>
+                        </TableCell>
+                        <TableCell align="right">
+                            <Typography variant='body2' color={customTheme.palette.primary.contrastText} align='left'>
+                                {row.quantity}
+                            </Typography>
+                        </TableCell>
+                        <TableCell align="right">
+                            <Typography variant='body2' color={customTheme.palette.primary.contrastText} align='left'>
+                                {row.price} $
+                            </Typography>
+                        </TableCell>
+                        <TableCell align="right">
+                            <Typography variant='body2' color={customTheme.palette.primary.contrastText} align='left'>
+                                <Button variant='outlined'
+                                    color='error'
+                                    endIcon={<DeleteOutlineOutlinedIcon />}
+                                    >
+                                    Sell
+                                </Button>
+                            </Typography>
+                        </TableCell>
+                    </StyledTableRow>
+                ))}
+            </TableBody>
+        </Table>
+    )
+}
---
+++ stock-market/ClientApp/src/fetchingData/FetchPortfolio.jsx
@@ -0,0 +1,108 @@
+﻿import React, { useEffect, useState } from 'react';
+import PortfolioTable from '../components/PortfolioComp/PortfolioTable';
+import CircularProgress from '@mui/material/CircularProgress';
+
+import axios from 'axios';
+
+
+export default function FetchTransactions() {
+    const [isLoading, setIsLoading] = useState(true);
+
+    const dummyData = [
+        {
+            Id: 0,
+            ticker: 'AAPL',
+            price: 254.32,
+            type: 'BUY',
+            quantity: 10,
+            timestampid: '2022-10-15T00:01:00Z',
+            Userid: 0,
+        },
+        {
+            Id: 1,
+            ticker: 'AAPL',
+            price: 254.32,
+            type: 'BUY',
+            quantity: 10,
+            timestampid: '2022-10-15T00:01:00Z',
+            Userid: 0,
+        },
+        {
+            Id: 2,
+            ticker: 'AAPL',
+            price: 254.32,
+            type: 'BUY',
+            quantity: 10,
+            timestampid: '2022-10-15T00:01:00Z',
+            Userid: 0,
+        },
+        {
+            Id: 3,
+            ticker: 'AAPL',
+            price: 254.32,
+            type: 'BUY',
+            quantity: 10,
+            timestampid: '2022-10-15T00:01:00Z',
+            Userid: 0,
+        },
+        {
+            Id: 4,
+            ticker: 'AAPL',
+            price: 254.32,
+            type: 'SELL',
+            quantity: 10,
+            timestampid: '2022-10-15T00:01:00Z',
+            Userid: 0,
+        },
+        {
+            Id: 5,
+            ticker: 'AAPL',
+            price: 254.32,
+            type: 'SELL',
+            quantity: 10,
+            timestampid: '2022-10-15T00:01:00Z',
+            Userid: 0,
+        },
+    ];
+
+    const [transactions, setTransactions] = useState([
+        {
+            Id: 0,
+            ticker: 'AAPL',
+            price: 254.32,
+            type: 'BUY',
+            quantity: 10,
+            timestampid: '2022-10-15T00:01:00Z',
+            Userid: 0,
+        }
+    ]);
+
+    useEffect(() => {
+        axios.get("transaction/listall")
+            .then(res => {
+                setTransactions(res.data);
+                console.log(res);
+
+                setIsLoading(false);
+            })
+    }, [])
+
+
+    return (
+        isLoading == true ?
+            <>
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: 'center'
+                    }}>
+                    <CircularProgress color="success" />
+                </div>
+            </>
+            :
+            <>
+                <PortfolioTable data={dummyData} />
+            </>
+    );
+}
