03fc5c905970a18bd8b6cf6a5414b7dfe3facee5
Kristian Smedsrød <krissmed@protonmail.com>
2022-10-31 04:47:01 +0100

    Refined the RespNav

--- stock-market/ClientApp/src/components/TopBar.jsx
+++ stock-market/ClientApp/src/components/TopBar.jsx
@@ -22,10 +22,9 @@ export const TopBar = ({ title }) => {
             elevation={0} //No shadow;
             sx={{
                 ml: drawerWidth + 'px',
-                mr: 2,
                 padding: 1,
                 width: 'auto',
-                backgroundColor: customTheme.palette.background.default,
+                backgroundColor: isMobile() ? customTheme.palette.primary.main : customTheme.palette.background.default,
                 display: 'flex',
                 flexDirection: 'row',
                 alignItems: 'center',
