76fe8e65021cc834368d27022479d4becfc834de
Kristian Smedsrød <krissmed@protonmail.com>
2022-10-31 04:20:14 +0100

    Reverted main.db

--- stock-market/ClientApp/src/pages/Dashboard.jsx
+++ stock-market/ClientApp/src/pages/Dashboard.jsx
@@ -3,149 +3,61 @@
 import { TopBar } from '../components/TopBar.jsx';
 import DashboardGraph from '../components/DashboardComp/DashboardGraph';
 import OverViewStocks from '../components/DashboardComp/OverViewStocks';
-import DashboardStocks from '../components/DashboardComp/DasboardStocks'
+
 import { isMobile } from './Layout'
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
-import { useTheme, styled } from '@mui/material/styles';
-import { Typography } from '@mui/material';
-import { Link } from "react-router-dom";
-import Box from '@mui/material/Box';
-import DashboardWatchlist from '../components/DashboardComp/DashboardWatchlist'
-import DashboardTransactions from '../components/DashboardComp/DashboardTransactions'
-
-
 
 
 
 export default function Dashboard() {
     const drawerWidth = isMobile() ? 0 : 220;
-    const customTheme = useTheme();
     return (
         <>
             <TopBar title='My Dashboard' />
 
-            <Box
-                sx={{
-                    marginLeft: drawerWidth + 'px'
-                }}
-            >
+
             <Container>
-                <Grid
-                    container spacing={5}
+                <Grid container spacing={1} sx={{
+                    ml: drawerWidth + 'px',
+                    width: 'auto'
+                }}>
 
-                >
+                    {/* <Grid item xs={12} sx={{
+                        minHeight: '120px',
+                        textAlign: 'center',
+                        alignContent: 'center'
+                    }}>
+                        
+                        </Grid>*/}
 
-                        <Grid item xs={12} md={8} zeroMinWidth sx={{
-                            minHeight: '150px',
-                            backgroundClip: 'content-box',
-                            backgroundColor: customTheme.palette.primary.main
-
+                    <Grid item xs={12} md={8} sx={{
+                            minHeight: '350px'
                         }}>
-                            <DashboardGraph noWrap />
+                            <DashboardGraph />
                         </Grid>
 
-                        <Grid item xs={12} md={4} sx={{
-                            minHeight: '150px',
-                            backgroundClip: 'content-box',
-                            backgroundColor: customTheme.palette.primary.main
-
-                        }}>
-                            <Typography
-                                variant='h5'
-                                sx={{
-                                    paddingTop: '8px',
-                                    paddingLeft: '8px',
-                                    color: 'white',
-                                    backgroundColor: customTheme.palette.primary.main
-                                }}
-                                >
-                                Stocks
-                            </Typography>
-                            <DashboardStocks />
-                            <Link to='/stocks'>
-                            <Typography
-                                variant='subtitle2'
-                                align='right'
-                                sx={{
-                                    paddingTop: '8px',
-                                    paddingRight: '8px',
-                                    paddingBottom: '8px',
-                                    color: customTheme.palette.primary.contrastText
-
-                                }}>
-                                View more➔
-                                </Typography>
-                            </Link>
+                    <Grid item xs={12} md={4} sx={{
+                            minHeight: '350px'
+                    }}>
+                        <h5>Stocks</h5>
                         </Grid>
 
-                    <Grid item xs={12} md={7.4} sx={{
-                            minHeight: '150px',
-                            backgroundClip: 'content-box',
-                            backgroundColor: customTheme.palette.primary.main
-
+                    <Grid item xs={12} md={6} sx={{
+                            minHeight: '150px'
                         }}>
-                            <Typography
-                                variant='h5'
-                                sx={{
-                                    paddingTop: '8px',
-                                    paddingLeft: '8px',
-                                    color: 'white',
-                                }}
-                            >
-                                Watchlist
-                            </Typography>
-                            <DashboardWatchlist />
-                            <Link to='/watchlist'>
-                            <Typography
-                                variant='subtitle2'
-                                align='right'
-                                sx={{
-                                    paddingTop: '8px',
-                                    paddingRight: '8px',
-                                    paddingBottom: '8px',
-                                    color: customTheme.palette.primary.contrastText
-
-                                }}>
-                                View more➔
-                                </Typography>
-                            </Link>
+                        <h5>Watchlist</h5>
                         </Grid>
 
-                        <Grid item xs={12} md={7.25} sx={{
-                            minHeight: '150px',
-                            backgroundClip: 'content-box',
-                            backgroundColor: customTheme.palette.primary.main
+                    <Grid item xs={12} md={6} sx={{
+                            minHeight: '150px'
                         }}>
-                            <Typography
-                                variant='h5'
-                                sx={{
-                                    paddingTop: '8px',
-                                    paddingLeft: '8px',
-                                    color: customTheme.palette.primary.contrastText
-                                }}>
-                                Transactions
-                            </Typography>
-                            <DashboardTransactions />
-                            <Link to='/transactions'>
-                            <Typography
-                                variant='subtitle2'
-                                align='right'
-                                sx={{
-                                    paddingTop: '8px',
-                                    paddingRight: '8px',
-                                    paddingBottom: '8px',
-                                    color: customTheme.palette.primary.contrastText
-
-                                }}>
-                                View more➔
-                                </Typography>
-                            </Link>
+                        <h5>Transactions</h5>
                         </Grid>
 
                 </Grid>
-                </Container>
-            </Box>
+            </Container>
+
          </>
         );
 }
