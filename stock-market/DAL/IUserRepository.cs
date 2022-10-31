bf492fd7119857e97f4ad8c879274e9a5291ac32
erlingstaff <erlingstaff@gmail.com>
2022-10-31 06:52:43 +0100

    IUser interface fix

--- stock-market/DAL/IUserRepository.cs
+++ stock-market/DAL/IUserRepository.cs
@@ -15,8 +15,6 @@ namespace stock_market.DAL
         Task<bool> DeleteUser(int id);
         Task<bool> EditUser(User editUser);
         Task<bool> EditUserBalance(User editUser);
-        
-
 
     }
 }
