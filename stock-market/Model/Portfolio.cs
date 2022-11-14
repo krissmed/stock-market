acf1f117ac98a460cf9a5b2875a99662100b1e76
Vetle Endrerud <endrerudvetle@gmail.com>
2022-11-14 19:00:17 +0100

    Added REGEX inn relevant models

--- stock-market/Model/Portfolio.cs
+++ stock-market/Model/Portfolio.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace stock_market.Model
 {
@@ -7,8 +9,11 @@ namespace stock_market.Model
     {
         public int id { get; set; }
         public User user { get; set; }
+        [RegularExpression(@"^[0-9]{2,20}$")]
         public double total_value { get; set; }
+        [RegularExpression(@"^[0-9]{2,20}$")]
         public double stock_value { get; set; }
+        [RegularExpression(@"^[0-9]{2,20}$")]
         public double liquid_value { get; set; }
         public List<BaseStockCounter> stock_counter { get; set; }
         public Timestamp timestamp { get; set; }
--- stock-market/Model/Transaction.cs
+++ stock-market/Model/Transaction.cs
@@ -9,10 +9,14 @@ namespace stock_market.Model
         //[Key]
         //[DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int id { get; set; }
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,20}$")]
         public string ticker { get; set; }
+        [RegularExpression(@"^[0-9]{2,20}$")]
         public double price { get; set; }
         public User user { get; set; }
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,20}$")]
         public string type { get; set; }
+        [RegularExpression(@"^[0-9]{2,20}$")]
         public int quantity { get; set; }
         public Timestamp timestamp { get; set; }
     }
